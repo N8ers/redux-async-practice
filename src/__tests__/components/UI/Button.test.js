64a5bb6c606a47dc746c80d1afff0c08a78ba62b
Nathan <nathanraysheryak@gmail.com>
2021-12-14 06:44:18 -0500

    move tests to __tests__ folder

--- src/__tests__/components/UI/Button.test.js
+++ src/__tests__/components/UI/Button.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Button from "./Button";
+import Button from "../../../components/UI/Button";
 
 describe("Greeting Component", () => {
   test("renders prop content", () => {

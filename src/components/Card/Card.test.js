825bd38b3e5a77480e1b3fc03ab48092a122daa9
Nathan <nathanraysheryak@gmail.com>
2021-12-16 07:45:21 -0500

    move tests back to component folder, __tests__ dir was causing problems

---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import React from "react";
+
+import Card from "./Card";
+
+describe("Default setup", () => {
+  // test("check init values", () => {
+  //   render(<Card />);
+  //   const card = screen.getByText("Count:");
+  //   expect(card).toBeInTheDocument();
+  // });
+  // test("check init snap", () => {
+  //   render(<Card />);
+  //   const card = screen.getByText("Count:");
+  //   expect(card).toMatchSnapshot();
+  // });
+});
+
+// describe("Redux and reactivity", () => {
+//   test("increment button", () => {});
+//   test("decrement button", () => {});
+// });
